4c3b9775f6d8e8eaf0fe313f9acb163377989f96
saltchicken <john.eicher89@gmail.com>
2026-05-21 17:13:12 -0700

    Init

--- src/agent_lab/sub_agents/calculator/tools.py
+++
@@ -1,8 +0,0 @@
-def add_numbers(a: float, b: float) -> float:
-    """Add two numbers together."""
-    return a + b
-
-
-def multiply_numbers(a: float, b: float) -> float:
-    """Multiply two numbers together."""
-    return a * b
