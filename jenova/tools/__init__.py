3a18015c1898c9a63af3248f6e3bc03a7c294bd5
saltchicken <john.eicher89@gmail.com>
2026-05-25 15:51:09 -0700

    Cleanup to init

--- jenova/tools/__init__.py
+++
@@ -1,5 +0,0 @@
-from .lights import turn_on_lights
-from .lights import turn_off_lights
-from .search import search_duckduckgo
-from .search import scrape_website
-from .memory import remember_user_fact
